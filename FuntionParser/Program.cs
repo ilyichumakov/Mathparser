78c634bbb1e8f34f5207a27ca64918d5649d2fb4
Ilya Chumakov <ilya.chumakov99@gmail.com>
2019-04-06 12:29:18 +0300

    let it be, some trash here

--- FuntionParser/Program.cs
+++ FuntionParser/Program.cs
@@ -23,6 +23,8 @@ namespace FuntionParser
                             return MakeOperation(full[opers.left_child], vals, full) - MakeOperation(full[opers.right_child],  vals, full);
                         case "*":
                             return MakeOperation(full[opers.left_child],  vals, full) * MakeOperation(full[opers.right_child],  vals, full);
+                        case "/":
+                            return MakeOperation(full[opers.left_child], vals, full) / MakeOperation(full[opers.right_child], vals, full);
                         case "^":
                             return Math.Pow(MakeOperation(full[opers.left_child], vals, full), MakeOperation(full[opers.right_child], vals, full));
                         default:
@@ -44,7 +46,7 @@ namespace FuntionParser
             }
         }    
                 
-        static Dictionary<string, int> op = new Dictionary<string, int>();
+        static Dictionary<string, int> op = new Dictionary<string, int>(); //соответствия операций и приоритетов
         
         public static string ReadExpression()
         {
@@ -58,15 +60,22 @@ namespace FuntionParser
             Dictionary<string, double> result = new Dictionary<string, double>(); //установим соответствие между именами переменных и их значениями
             foreach(string variable in variables)
             {
-                if (!op.ContainsKey(variable) && !result.ContainsKey(variable)) //если не оператор и не считано ранее
+                bool isNumber = Double.TryParse(variable, out double number);
+                if (!op.ContainsKey(variable) && !result.ContainsKey(variable) && !isNumber) //если не оператор, не считано ранее и не число
                 {
                     string request = "Значение " + variable + " = ";
                     Console.Write(request);
                     result.Add(variable, Double.Parse(Console.ReadLine())); //добавим
                 }
+                else if(isNumber) // если число
+                {
+                    result.Add(variable, number); //добавим
+                }
             }
             return result;
         }
+
+        public static List<int> Priorities = new List<int>(); // список приоритетов внесенных операций
 
         public static List<string> ParseExpression(string expression)
         {
@@ -86,27 +95,66 @@ namespace FuntionParser
                     someChars = "";
                     someChars += c; // оператор
                     result.Add(someChars); //добавим оператор
+                    Priorities.Add(op[someChars]); // приоритет операции занесем в выделенный для этого список
                     someChars = ""; //подготовили для следующего ввода
                 }
                 last = c.ToString(); // резерв для конца строки
             }
             if (op.ContainsKey(last)) throw new FormatException("Last symbol was an operator!"); // если в конце стоял оператор, нас ПОКА ЧТО не устраивает
-            else result.Add(someChars); // если была переменная, добавим
+            else result.Add(someChars); // если была переменная, добавим            
             return result;
+        }
+
+        public static List<string> SortOperators(List<string> parsed, List<int> priorities, Dictionary<string, int> op)
+        {
+            bool sorted = false;
+            while (!sorted)
+            {
+                int curOperator = 0;
+                int i = 0;
+                while(i<parsed.Count)
+                {
+                    string s = parsed[i];
+                    if (op.ContainsKey(s) && curOperator > 0)
+                    {
+                        if (priorities[curOperator] > priorities[curOperator - 1])
+                        {
+                            string tempStr = s;
+                            parsed[i] = parsed[i - 2];
+                            parsed[i - 2] = tempStr;
+                            tempStr = parsed[i + 1];
+                            parsed[i + 1] = parsed[i - 1];
+                            parsed[i - 1] = tempStr;
+                            i = -1;
+                            int changePriority = priorities[curOperator];
+                            priorities[curOperator] = priorities[curOperator - 1];
+                            sorted = false;
+                            curOperator = 0;
+                        }
+                    }
+                    else if (op.ContainsKey(s)) curOperator++;
+                    i++;
+                    if (i == parsed.Count) sorted = true;
+                }
+            }
+            return parsed;
         }
 
         static void Main(string[] args)
         {
             Tree[] OperationTree = new Tree[100];
-            op.Add("+", 1);
-            op.Add("-", 1);
-            op.Add("*", 2);
-            op.Add("/", 2);
-            op.Add("^", 3);
-            op.Add("!", 4);
+            op.Clear();
+            Priorities.Clear();
+            op.Add("+", 6);
+            op.Add("-", 5);
+            op.Add("*", 3);
+            op.Add("/", 4);
+            op.Add("^", 2);
+            op.Add("!", 1);
             try
             {                
                 List<string> parsed = ParseExpression(ReadExpression());
+                parsed = SortOperators(parsed, Priorities, op);
                 if (parsed.First() != "0")
                 {
                     int i = 0;
