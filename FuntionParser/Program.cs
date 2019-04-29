b8bd5bb1ea46518533edd65b595ce98703b4f6c2
Ilya Chumakov <ilya.chumakov99@gmail.com>
2019-04-29 08:33:00 +0300

    before restructure

--- FuntionParser/Program.cs
+++ FuntionParser/Program.cs
@@ -7,8 +7,7 @@ using System.Threading.Tasks;
 namespace FuntionParser
 {
     class Program
-    {        
-        
+    {                
         static double MakeOperation(Tree opers, Dictionary<string, double> vals, Tree[] full)
         {
             if (op.ContainsKey(opers.value))
@@ -80,6 +79,7 @@ namespace FuntionParser
         public static List<string> ParseExpression(string expression)
         {
             Priorities.Clear();
+            //op.Clear();
             string someChars = "";
             List<string> result = new List<string>();
             string last = "1";
@@ -162,10 +162,7 @@ namespace FuntionParser
         }
 
         static void Main(string[] args)
-        {
-            Tree[] OperationTree = new Tree[100];
-            op.Clear();
-           // Priorities.Clear();
+        {           
             op.Add("+", 6);
             op.Add("-", 5);
             op.Add("*", 3);
@@ -173,49 +170,19 @@ namespace FuntionParser
             op.Add("^", 2);
             op.Add("!", 1);
             bool end = false;
+
             while (!end)
             {
                 try
                 {
+                    Tree[] OperationTree = new Tree[100];
                     string input = ReadExpression();
                     List<string> parsed = ParseExpression(input);
                     parsed = SortOperators(parsed, Priorities, op);
                     if (parsed.First() != "0")
                     {
                         int i = 0;
-                        bool rightChildExpected = false;
-                        string leftOperand = "";
-
-
-                        /*foreach (string oper in parsed)  //old but works, no priority
-                        {
-                            if (!op.ContainsKey(oper)) // если не оператор
-                            {
-                                if (i == 0 || !rightChildExpected) // в начале всегда должна быть переменная, здесь же все левые ветви
-                                {
-                                    leftOperand = oper; // левая ветвь
-                                    if (i == 0) OperationTree[1].Init(-1, -1, oper, 0); //если первый элемент
-                                    rightChildExpected = true;
-                                }
-                            }
-                            else
-                            {
-                                if (i == 1) OperationTree[0].Init(1, 2, oper, -1);
-                                else
-                                {
-                                    OperationTree[i - 1].Init(i, i + 1, oper, i - 3);
-                                    OperationTree[i].Init(-1, -1, leftOperand, i - 1);
-                                }
-                                rightChildExpected = false;
-                            }
-                            i++;
-                        }
-                        OperationTree[i - 1].Init(-1, -1, parsed.Last(), i - 3); // последний*/
-
-
-
-                        //with priority
-                        //int maxPriority = Priorities.Max();
+                                                                        
                         bool decomposed = false;
                         OperationTree[0].Init(-1, -1, input, -1);
                         while (!decomposed)
@@ -258,10 +225,7 @@ namespace FuntionParser
                             }
                             i++;
                         }
-                       
-
-
-                       
+                                              
                         Dictionary<string, double> vals = ReadVals(parsed); // считываем значения переменных
                         double result = MakeOperation(OperationTree[0], vals, OperationTree); // запускаем обход дерева от корня
                         Console.WriteLine(result.ToString()); //вывод результата                    
@@ -272,7 +236,6 @@ namespace FuntionParser
                 {
                     Console.WriteLine(ex.Message);
                 }
-                //Console.ReadKey();
             }
         }
     }
---
+++ Tests/ParseTests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FuntionParser;
+using NUnit.Framework;
+
+namespace Tests
+{
+    [TestFixture]
+    public class ParseTests
+    {
+        [SetUp]
+        public void CreateFields()
+        {
+
+        }
+        [Test]
+        public void SimpleSum()
+        {
+            string expression = "x+y";
+            Dictionary<string, double> vals = new Dictionary<string, double>();
+            vals.Add("x", 5);
+            vals.Add("y", 2);
+            double expected = 7;
+        }
+    }
+}
